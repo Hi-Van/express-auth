a6941a19c6f85f94a841743e005c4350c5bab5f6
Hi-Van <sirisoukvanouphon@gmail.com>
2024-01-26 18:43:09 -0600

    enable CORS policies

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import v1Router from "./routes/v1/index.js";
 const port = 8000;
 const app: Express = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/v1", v1Router);
